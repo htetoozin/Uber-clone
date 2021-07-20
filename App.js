2dfc97cae7266d1823e3e9ed8e493415acbaf0ce
Htet Oo Zin <htetoo.zin09@gmail.com>
2021-07-20 16:37:45 +0630

    Install vector icon

--- App.js
+++ App.js
@@ -26,6 +26,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -71,6 +73,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
+            <Icon name="rocket" size={30} color="#900" />
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
